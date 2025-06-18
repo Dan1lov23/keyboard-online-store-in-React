0e32daf983f55ca1c81a5c21d1507b00ccdea960
kitik <svy1dan@gmail.com>
2025-06-18 15:18:39 +0300

    Directory with products for catalog and with images for this products

---
+++ src/products/products array/products array.tsx
@@ -0,0 +1,366 @@
+const productsArray = [
+    {
+        name: "8BitDo Retro Mechanical Keyboard - Fami Edition",
+        price: 7299,
+        img: "src/products/products gallery by brands/8BitDo/8bitdo1.png",
+        id: 1,
+        type: "механическая",
+        specificity: "Офисная",
+        brand: "8BitDo",
+        switchModel: "Kailh Box White V2",
+        width: "376.6 мм",
+        height: "46.8 мм",
+        connectionType: "провод/безпровод",
+        keyBacklight: "нет",
+        lowProfile: "no",
+        itemCounter: 1,
+        iconsArray: [
+           "https://cdn-icons-png.flaticon.com/128/88/88014.png",
+           "https://cdn-icons-png.flaticon.com/128/4735/4735258.png",
+            "https://cdn-icons-png.flaticon.com/128/500/500122.png",
+        ],
+        imgForSlider: [
+            "src/products/products gallery by brands/8BitDo/8BitDo slider 1/8bitDosl1.webp",
+            "src/products/products gallery by brands/8BitDo/8BitDo slider 1/8bitDosl2.webp.webp",
+            "src/products/products gallery by brands/8BitDo/8BitDo slider 1/8bitDosl3.webp.webp",
+            "src/products/products gallery by brands/8BitDo/8BitDo slider 1/8bitDosl4.webp.webp",
+            "src/products/products gallery by brands/8BitDo/8BitDo slider 1/8bitDosl5.webp.webp",
+        ]
+    },
+    {
+        name: "8BitDo Retro Mechanical Keyboard - M Edition",
+        price: 6799,
+        img: "src/products/products gallery by brands/8BitDo/8bitdo2.png",
+        id: 2,
+        type: "механическая",
+        specificity: "Офисная",
+        brand: "8BitDo",
+        switchModel: "Kailh Box White V2",
+        width: "376.6 мм",
+        height: "46.8 мм",
+        connectionType: "провод/безпровод",
+        keyBacklight: "нет",
+        lowProfile: "no",
+        itemCounter: 1,
+        iconsArray: [
+            "https://cdn-icons-png.flaticon.com/128/88/88014.png",
+            "https://cdn-icons-png.flaticon.com/128/4735/4735258.png",
+            "https://cdn-icons-png.flaticon.com/128/500/500122.png",
+        ],
+        imgForSlider: [
+            "src/products/products gallery by brands/8BitDo/8bitDo slider 2/8bitDosl1.webp.webp",
+            "src/products/products gallery by brands/8BitDo/8bitDo slider 2/8bitDosl2.webp.webp",
+            "src/products/products gallery by brands/8BitDo/8bitDo slider 2/8bitDosl3.webp.webp",
+            "src/products/products gallery by brands/8BitDo/8bitDo slider 2/8bitDosl4.webp.webp",
+            "src/products/products gallery by brands/8BitDo/8bitDo slider 2/8bitDosl5.webp.webp"
+        ]
+    },
+    {
+        name: "8BitDo Retro Mechanical Keyboard - N Edition",
+        price: 6699,
+        img: "src/products/products gallery by brands/8BitDo/8bitdo3.png",
+        id: 3,
+        type: "механическая",
+        specificity: "Офисная",
+        brand: "8BitDo",
+        switchModel: "Kailh Box White V2",
+        width: "376.6 мм",
+        height: "46.8 мм",
+        connectionType: "провод/безпровод",
+        keyBacklight: "нет",
+        lowProfile: "no",
+        itemCounter: 1,
+        iconsArray: [
+            "https://cdn-icons-png.flaticon.com/128/88/88014.png",
+            "https://cdn-icons-png.flaticon.com/128/4735/4735258.png",
+            "https://cdn-icons-png.flaticon.com/128/500/500122.png",
+        ],
+        imgForSlider: [
+            "src/products/products gallery by brands/8BitDo/8bitDo slider 3/8bitDosl1.webp",
+            "src/products/products gallery by brands/8BitDo/8bitDo slider 3/8bitDosl2.webp",
+            "src/products/products gallery by brands/8BitDo/8bitDo slider 3/8bitDosl3.webp",
+            "src/products/products gallery by brands/8BitDo/8bitDo slider 3/8bitDosl4.webp",
+            "src/products/products gallery by brands/8BitDo/8bitDo slider 3/8bitDosl5.webp"
+        ],
+    },
+
+
+    {
+        name: "Logitech MX Keys S",
+        price: 12499,
+        img: "src/products/products gallery by brands/Logitech/log1.webp",
+        id: 4,
+        type: "мембранная",
+        specificity: "Офисная",
+        brand: "logitech",
+        switchModel: "",
+        width: "430 мм",
+        height: "20.5 мм",
+        connectionType: "провод/безпровод",
+        keyBacklight: "да",
+        lowProfile: "yes",
+        itemCounter: 1,
+        iconsArray: [
+            "https://cdn-icons-png.flaticon.com/128/500/500122.png",
+        ],
+        imgForSlider: [
+            "src/products/products gallery by brands/Logitech/log slider 1/logSl1.webp",
+            "src/products/products gallery by brands/Logitech/log slider 1/logSl2.webp",
+            "src/products/products gallery by brands/Logitech/log slider 1/logSl3.webp",
+            "src/products/products gallery by brands/Logitech/log slider 1/logSl4.webp",
+            "src/products/products gallery by brands/Logitech/log slider 1/logSl5.webp",
+        ],
+    },
+    {
+        name: " Logitech MX Keys Mini Pale",
+        price: 6699,
+        img: "src/products/products gallery by brands/Logitech/log2.webp",
+        id: 5,
+        type: "мембранная",
+        specificity: "Офисная",
+        brand: "logitech",
+        switchModel: "",
+        width: "296 мм",
+        height: "21 мм",
+        connectionType: "провод/безпровод",
+        keyBacklight: "да",
+        lowProfile: "yes",
+        itemCounter: 1,
+        iconsArray: [
+            "https://cdn-icons-png.flaticon.com/128/500/500122.png",
+        ],
+        imgForSlider: [
+            "src/products/products gallery by brands/Logitech/log slider 2/logSl1.webp",
+            "src/products/products gallery by brands/Logitech/log slider 2/logSl2.webp",
+            "src/products/products gallery by brands/Logitech/log slider 2/logSl3.webp",
+            "src/products/products gallery by brands/Logitech/log slider 2/logSl4.webp",
+            "src/products/products gallery by brands/Logitech/log slider 2/logSl5.webp",
+        ],
+    },
+    {
+        name: "Logitech Keys-To-Go 2",
+        price: 9999,
+        img: "src/products/products gallery by brands/Logitech/log7.jpg",
+        id: 6,
+        type: "механическая",
+        specificity: "Офисная",
+        brand: "logitech",
+        switchModel: "",
+        width: "105 мм",
+        height: "250 мм",
+        itemCounter: 1,
+        connectionType: "безпровод",
+        keyBacklight: "нет",
+        lowProfile: "yes",
+        iconsArray: [
+            "https://cdn-icons-png.flaticon.com/128/500/500122.png",
+        ],
+        imgForSlider: [
+            "src/products/products gallery by brands/Logitech/log slider 3/logSl1.webp",
+            "src/products/products gallery by brands/Logitech/log slider 3/logSl2.webp",
+            "src/products/products gallery by brands/Logitech/log slider 3/logSl3.webp",
+            "src/products/products gallery by brands/Logitech/log slider 3/logSl4.webp",
+            "src/products/products gallery by brands/Logitech/log slider 3/logSl1.webp",
+        ],
+    },
+    {
+        name: "Logitech G Pro",
+        price: 12699,
+        img: "src/products/products gallery by brands/Logitech/log3.webp",
+        id: 7,
+        type: "механическая",
+        specificity: "Игровая",
+        brand: "logitech",
+        switchModel: "Logitech GX Blue",
+        width: "361 мм",
+        height: "34.3 мм",
+        connectionType: "безпровод",
+        keyBacklight: "да",
+        lowProfile: "no",
+        itemCounter: 1,
+        iconsArray: [
+            "https://cdn-icons-png.flaticon.com/128/500/500122.png",
+        ],
+        imgForSlider: [
+            "src/products/products gallery by brands/Logitech/log slider 4/logSl1.webp",
+            "src/products/products gallery by brands/Logitech/log slider 4/logSl2.webp",
+            "src/products/products gallery by brands/Logitech/log slider 4/logSl3.webp",
+            "src/products/products gallery by brands/Logitech/log slider 4/logSl4.webp",
+            "src/products/products gallery by brands/Logitech/log slider 4/logSl5.webp",
+        ]
+    },
+    {
+        name: "Logitech G913",
+        price: 21199,
+        id: 8,
+        img: "src/products/products gallery by brands/Logitech/log4.webp",
+        type: "механическая",
+        specificity: "Игровая",
+        brand: "logitech",
+        switchModel: "Logitech GL Tactile",
+        width: "475 мм",
+        height: "22 мм",
+        connectionType: "безпровод",
+        keyBacklight: "да",
+        lowProfile: "no",
+        itemCounter: 1,
+        iconsArray: [
+            "https://cdn-icons-png.flaticon.com/128/88/88014.png",
+            "https://cdn-icons-png.flaticon.com/128/4735/4735258.png",
+            "https://cdn-icons-png.flaticon.com/128/500/500122.png",
+        ],
+        imgForSlider: [
+            "src/products/products gallery by brands/Logitech/log slider 7/logSl1.webp",
+            "src/products/products gallery by brands/Logitech/log slider 7/logSl2.webp",
+            "src/products/products gallery by brands/Logitech/log slider 7/logSl3.webp",
+            "src/products/products gallery by brands/Logitech/log slider 7/logSl4.webp",
+            "src/products/products gallery by brands/Logitech/log slider 7/logSl5.webp",
+        ]
+    },
+    {
+        name: "Logitech G913 TKL",
+        price: 21199,
+        id: 9,
+        img: "src/products/products gallery by brands/Logitech/log5.webp",
+        type: "механическая",
+        specificity: "Игровая",
+        brand: "logitech",
+        switchModel: "Logitech GL Clicky",
+        width: "368 мм",
+        height: "22 мм",
+        connectionType: "провод/безпровод",
+        keyBacklight: "да",
+        lowProfile: "no",
+        itemCounter: 1,
+        iconsArray: [
+            "https://cdn-icons-png.flaticon.com/128/88/88014.png",
+            "https://cdn-icons-png.flaticon.com/128/4735/4735258.png",
+            "https://cdn-icons-png.flaticon.com/128/500/500122.png",
+        ],
+        imgForSlider: [
+            "src/products/products gallery by brands/Logitech/log slider 6/logSl1.webp",
+            "src/products/products gallery by brands/Logitech/log slider 6/logSl2.webp",
+            "src/products/products gallery by brands/Logitech/log slider 6/logSl3.webp",
+            "src/products/products gallery by brands/Logitech/log slider 6/logSl4.webp",
+            "src/products/products gallery by brands/Logitech/log slider 6/logSl5.webp",
+        ],
+    },
+    {
+        name: "Logitech G913 TKL",
+        price: 21199,
+        id: 10,
+        img: "src/products/products gallery by brands/Logitech/log6.webp",
+        type: "механическая",
+        specificity: "Игровая",
+        brand: "logitech",
+        connectionType: "провод",
+        keyBacklight: "да",
+        lowProfile: "no",
+        itemCounter: 1,
+        switchModel: "Logitech GL Clicky",
+        width: "368 мм",
+        height: "22 мм",
+        iconsArray: [
+            "https://cdn-icons-png.flaticon.com/128/500/500122.png",
+        ],
+        imgForSlider: [
+            "src/products/products gallery by brands/Logitech/log slider 5/logSl1.webp",
+            "src/products/products gallery by brands/Logitech/log slider 5/logSl2.webp",
+            "src/products/products gallery by brands/Logitech/log slider 5/logSl3.webp",
+            "src/products/products gallery by brands/Logitech/log slider 5/logSl4.webp",
+            "src/products/products gallery by brands/Logitech/log slider 5/logSl5.webp",
+        ]
+    },
+    {
+        name: "Keychron Lemokey L3",
+        price: 28999,
+        img: "src/products/products gallery by brands/keyhron/keyhron1.webp",
+        id: 11,
+        type: "механическая",
+        specificity: "Игровая",
+        brand: "Keychron",
+        switchModel: "Gateron Jupiter Red",
+        width: "401 мм",
+        height: "30 мм",
+        connectionType: "провод/безпровод",
+        keyBacklight: "да",
+        lowProfile: "no",
+        itemCounter: 1,
+        iconsArray: [
+            "https://cdn-icons-png.flaticon.com/128/88/88014.png",
+            "https://cdn-icons-png.flaticon.com/128/4735/4735258.png",
+            "https://cdn-icons-png.flaticon.com/128/500/500122.png",
+        ],
+        imgForSlider: [
+            "src/products/products gallery by brands/keyhron/keychron slider 1/keySl1.webp",
+            "src/products/products gallery by brands/keyhron/keychron slider 1/keySl2.webp",
+            "src/products/products gallery by brands/keyhron/keychron slider 1/keySl3.webp",
+            "src/products/products gallery by brands/keyhron/keychron slider 1/keySl4.webp",
+            "src/products/products gallery by brands/keyhron/keychron slider 1/keySl5.webp",
+        ]
+    },
+    {
+        name: "Keychron Q5 Max",
+        price: 24599,
+        img: "src/products/products gallery by brands/keyhron/keyhron2.webp",
+        id: 12,
+        type: "механическая",
+        specificity: "Игровая",
+        brand: "Keychron",
+        switchModel: "Gateron Jupiter Banana",
+        width: "408 мм",
+        height: "30 мм",
+        connectionType: "провод/безпровод",
+        keyBacklight: "да",
+        lowProfile: "no",
+        itemCounter: 1,
+        iconsArray: [
+            "https://cdn-icons-png.flaticon.com/128/88/88014.png",
+            "https://cdn-icons-png.flaticon.com/128/4735/4735258.png",
+            "https://cdn-icons-png.flaticon.com/128/500/500122.png",
+        ],
+        imgForSlider: [
+            "src/products/products gallery by brands/keyhron/keychron slider 2/keySl1.webp",
+            "src/products/products gallery by brands/keyhron/keychron slider 2/keySl2.webp",
+            "src/products/products gallery by brands/keyhron/keychron slider 2/keySl3.webp",
+            "src/products/products gallery by brands/keyhron/keychron slider 2/keySl4.webp",
+            "src/products/products gallery by brands/keyhron/keychron slider 2/keySl5.webp",
+        ]
+    },
+];
+
+export {productsArray};
+
+const allProductsForSearch = () => {
+    const newArray = [];
+    for (let a = 0; a < productsArray.length; a++) {
+        newArray.push(productsArray[a].name);
+    }
+    return newArray;
+}
+
+export {allProductsForSearch};
+
+const allBrandsForSearch = () => {
+    const newArray:any = [];
+    for (let a = 0; a < productsArray.length; a++) {
+        if (!newArray.includes(productsArray[a].brand)) {
+            newArray.push(productsArray[a].brand);
+        }
+    }
+    return newArray;
+}
+
+export {allBrandsForSearch};
+
+const allProductsBrands = () => {
+    const newArray:any = [];
+    for (let a = 0; a < productsArray.length; a++) {
+        if (!newArray.includes(productsArray[a].brand)) {
+            newArray.push(productsArray[a].brand);
+        }
+    }
+    return newArray;
+}
+
+export {allProductsBrands};
