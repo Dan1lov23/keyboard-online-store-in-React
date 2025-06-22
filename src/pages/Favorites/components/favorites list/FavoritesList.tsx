a9406818e32c4b7f5084e9b703c30bfe29e93ac2
kitik <svy1dan@gmail.com>
2025-06-22 11:50:38 +0300

    Favorites directory

---
+++ src/pages/Favorites/components/favorites list/FavoritesList.tsx
@@ -0,0 +1,59 @@
+import "./favoritesList.css";
+
+import {useEffect} from "react";
+
+import {useDispatch, useSelector} from "react-redux";
+
+import ItemCart from "../../../Catalog/components/item cart/ItemCart.tsx"
+
+export default function FavoritesList() {
+
+    const dispatch = useDispatch();
+    const favorites = useSelector((state:any) => state.favorites);
+    const itemsArray = useSelector((state:any) => state.itemsArray);
+
+    const addItemFunction = (item:any) => {
+        if (itemsArray.some((item: any) => item.id === item.id)) {
+            dispatch({ type: "INCREMENT_PRODUCT_COUNTER", payload: { id: item.id } });
+            console.log("Товар добавлен");
+        } else {
+            dispatch({ type: 'ADD_PRODUCT', payload: item });
+            console.log("Такой товар уже есть в корзине");
+        }
+        dispatch({type: "INCREMENT_SUM", payload: item.price});
+    }
+
+    useEffect(() => {
+        fetch(`http://localhost:1405/api/getFavorites`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(favorites),
+        })
+    }, [favorites]);
+
+    return (
+        <>
+            <div className="favoritesListMain">
+                <div className="favoritesPageTitle">
+                    <h1>Избранное</h1>
+                </div>
+                {favorites.length === 0 ? (
+                    <div className="clearFavorites">
+                        <p>Тут пусто : (</p>
+                        <p>Добавьте понравившийся товар</p>
+                    </div>
+                ) : (
+                    <div className="productsList">
+                        {favorites.map((item: any) => (
+                            <div key={item.id}>
+                                <ItemCart item={item} addItemFunction={() => addItemFunction(item)}/>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </>
+    )
+}
---
+++ src/pages/Favorites/page/Favorites.tsx
@@ -0,0 +1,11 @@
+import FavoritesList from "../components/favorites list/FavoritesList.tsx";
+
+export default function Favorites() {
+    return (
+        <>
+            <div className="favoritesArray">
+                <FavoritesList />
+            </div>
+        </>
+    )
+}
